3ab05c1a7e8973f01dcddcf36b82ebe02b6723e2
Tatiane Pires <tatiane@tatianepires.net>
2021-07-23 02:04:11 -0300

    Compile CSS from Sass and update Font Awesome

--- inc/frontend-scripts.php
+++ inc/frontend-scripts.php
@@ -9,20 +9,26 @@ function tps_frontend_scripts() {
   // Parent theme's CSS
   wp_enqueue_style('twentyseventeen', get_template_directory_uri() . '/style.css' );
 
-  // Google Fonts
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans+Condensed:300|Raleway');
+  $third_party_styles = array(
+    'google-fonts' => array(
+      'src' => 'https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans+Condensed:300|Raleway',
+    ),
+    'font-awesome' => array(
+      'src' => get_stylesheet_directory_uri() . '/third-party/font-awesome/css/fontawesome.min.css',
+    ),
+    'font-awesome-light' => array(
+      'src' => get_stylesheet_directory_uri() . '/third-party/font-awesome/css/light.min.css',
+    ),
+  );
 
-  // Font Awesome
-  wp_enqueue_style('font-awesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  
+  // Enqueue third party styles
+  foreach($third_party_styles as $style_name => $style_data) {
+    wp_enqueue_style($style_name, $style_data['src'], array(), null);
+  }
+
   // Theme's CSS
   wp_enqueue_style('tps', get_stylesheet_directory_uri() . '/css/tps.min.css', array('twentyseventeen'));
 
-  // Remove jQuery 1.12.4
-  wp_deregister_script( 'jquery' );
-  // Add jQuery 2.2.4
-  wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', array(), '2.2.4', true);
-  
   if( is_front_page() ) {
     wp_enqueue_script('smooth-scroll', get_stylesheet_directory_uri() . '/js/smooth-scroll.min.js', array('jquery'), false, true);
   }
