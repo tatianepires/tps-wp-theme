7619cd35cc12bc455b6a8c746d402042b335411f
Tatiane Pires <tatiane@tatianepires.net>
2021-09-09 22:16:17 -0300

    Update social links on AMP footer

--- amp/footer.php
+++ amp/footer.php
@@ -2,8 +2,11 @@
 	<div>
 		<h2><?php echo esc_html( $this->get( 'blog_name' ) ); ?></h2>
 		<p class="social-icons">
-			<a href="https://twitter.com/tatianepires">@tatianepires</a>
-      <a href="https://www.facebook.com/tatianepiresblog">fb.com/tatianepiresblog</a>
+			<a href="https://twitter.com/tatianepires">Twitter</a>
+			<a href="https://instagram.com/tatianepires.tps">Instagram</a>
+			<a href="https://www.facebook.com/tatianepires.tps">Facebook</a>
+			<a href="https://linkedin.com/in/tatianepiress">LinkedIn</a>
+			<a href="https://github.com/tatianepires">GitHub</a>
 		</p>
 		<a href="#top" class="back-to-top"><?php _e( 'Back to top', 'amp' ); ?></a>
 	</div>
